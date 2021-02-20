81f43e056473bb9cb63117b962a7728b7cc7daa9
Gabriel <koymattk@gmail.com>
2021-02-20 18:46:11 -0300

    adicionando rota para criar novo artigo e salvar no banco de dados

--- src/controllers/categories/CategoiresController.js
+++ src/controllers/categories/CategoiresController.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const categories = require('../../models/Categories');
+const slugify = require('slugify');
+
 
 router.get('/admin/new', (req,res)=>{
-    res.render('admin/categories/new')
+    res.render('admin/categories/new');
 });
 
+router.post('/save', (req,res)=>{
+    const { title } = req.body;
+    if (title !== '') {
+        categories.create({
+            title,
+            slug: slugify(title)
+        }).then(()=>{
+            res.redirect('/');
+        });
+    }else{
+        es.redirect('/categories/admin/new')
+    }
+});
 module.exports = router;
 
