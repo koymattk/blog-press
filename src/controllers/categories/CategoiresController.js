bb28b39afc4c0f4d759363dc159a277707c783a4
Gabriel <koymattk@gmail.com>
2021-02-20 13:15:54 -0300

    adicionando formulario de envio

--- src/controllers/categories/CategoiresController.js
+++ src/controllers/categories/CategoiresController.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res)=>{
-    res.send("rota de categorias")
+router.get('/admin/new', (req,res)=>{
+    res.render('admin/categories/new')
 });
 
 module.exports = router;
