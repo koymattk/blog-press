af51dfeca89343e39a60de51eecb703ed19e8e11
Gabriel <koymattk@gmail.com>
2021-02-22 12:05:02 -0300

    criango formulario para criar um novo artigo

--- src/controllers/articles/ArticlesController.js
+++ src/controllers/articles/ArticlesController.js
@@ -5,5 +5,9 @@ router.get('/', (req,res)=>{
     res.send("rota de Artigos");
 });
 
+router.get('/admin/new', (req, res)=> {
+    res.render('admin/articles/new');
+})
+
 module.exports = router;
 
