a38bb974348600f4a8d774db8ea01fb182857d46
Gabriel <koymattk@gmail.com>
2021-02-22 15:01:00 -0300

    adicionando seleção de categoria de um artigo

--- src/controllers/articles/ArticlesController.js
+++ src/controllers/articles/ArticlesController.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
+const Category = require('../../models/Categories')
 router.get('/', (req,res)=>{
     res.send("rota de Artigos");
 });
 
 router.get('/admin/new', (req, res)=> {
-    res.render('admin/articles/new');
+    Category.findAll().then(category =>{
+        res.render('admin/articles/new', {category});
+    })
 })
 
 module.exports = router;
