25b936caa3b0202bef4acf796edae69775cc3faa
Gabriel <koymattk@gmail.com>
2021-02-24 17:03:11 -0300

    criando model usuarios

--- src/controllers/articles/ArticlesController.js
+++ src/controllers/articles/ArticlesController.js
@@ -93,7 +93,7 @@ router.get('/page/:num', (req,res) => {
     if (isNaN(page) || page == 1) {
         offset = 0
     }else{
-        offset = parseInt(page) * 4;
+        offset = (parseInt(page) - 1) * 4;
 
     }
 
