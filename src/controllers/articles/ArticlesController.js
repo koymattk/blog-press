ec44b04cddded25ff9bb2db0aafd13adbbeee600
Gabriel <koymattk@gmail.com>
2021-02-24 13:48:59 -0300

    criando edição de artigos

--- src/controllers/articles/ArticlesController.js
+++ src/controllers/articles/ArticlesController.js
@@ -47,5 +47,45 @@ router.post('/admin/delete/:id', (req,res)=> {
     }
 });
 
+router.get('/admin/edit/:id', (req,res)=>{
+    const id = req.params.id;
+    console.log(id)
+    if (id !== undefined) {
+        Article.findByPk(id).then(article => {
+            if (article === null) {
+                res.redirect('/articles/admin');
+            }
+            Category.findAll().then(category => {
+
+                res.render('admin/articles/edit', {article,category})
+            })
+        }).catch(erro =>{
+            console.log(erro)
+        })
+        
+    } else {
+        res.redirect('/articles/admin');
+    }
+})
+
+router.post('/admin/edit/:id', (req,res)=>{
+    const id = req.params.id;
+    const {title, body} = req.body;
+    if (id !== undefined) {
+        if (isNaN(id)) {
+            res.redirect('/articles/admin')  
+        }
+        Article.update({title:title, body:body, slug:slugify(title)},{
+            where:{
+                id:id
+            }
+        }).then(category => {
+            res.redirect('/articles/admin');
+        })
+    }else{
+        res.redirect('/articles/admin');
+    }
+})
+
 module.exports = router;
 
